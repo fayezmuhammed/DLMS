92a3a21c5bf8550a2867a642936c0f7a02d3dae4
Fayez Muhammed <117552186+fayezmuhammed@users.noreply.github.com>
2025-11-29 23:55:31 +0530

    fixed frontend url on backend

--- backend/server.js
+++ backend/server.js
@@ -35,7 +35,7 @@ const startServer = async () => {
     
     // CORS setup
     app.use(cors({
-      origin: ['http://localhost:5173', 'http://localhost:5174','https://dlms-backend-gz41.onrender.com'],
+      origin: ['http://localhost:5173', 'http://localhost:5174','https://dlms-two.vercel.app'],
       credentials: true,
       methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
       allowedHeaders: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
